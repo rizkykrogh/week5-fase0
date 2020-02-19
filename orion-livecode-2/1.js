8e360254d06a6e2212c6b7af21e4394c4ef78432
rizkykrogh <rizky.krogh@icloud.com>
2020-02-19 14:41:48 +0700

    Initial commit

---
+++ orion-livecode-2/1.js
@@ -0,0 +1,32 @@
+/**
+Diberikan sebuah function subset() yang menerima 2 parameter berisi
+array. Yaitu arr1 dan arr2 yang keduanya berisi angka.
+Function subset akan mengecek apakah arr1 merupakan subset dari arr2.
+Artinya, semua angka di arr1 harus berada di arr2.
+Angka di arr1 adalah angka unik, sehingga tidak ada angka yang muncul
+2 kali di arr1.
+
+RULES:
+WAJIB MENULISKAN PSEUDOCODE SEBELUM MEMBUAT CODE
+Dilarang menggunakan built-in function selain .push()
+
+*/
+
+//Tulislah Pseudocode disini
+
+function subset(arr1,arr2) {
+    arr1 = []
+    arr2 = []
+ //tulis function disini
+ for(i = 0; i < arr1.length; i++)
+    for(j = 0; j < i; j++)
+ for(i = 0; i < arr2.length; i++)
+    for(j = 0; j < i; j++)
+    if (arr2.length[i][j] = arr2.length[i][j])
+}
+
+console.log(subset([3,9,5],[1, 3, 9, 5, 8, 44])); //true
+console.log(subset([1,3], [1,3,5,7,8])) //true
+console.log(subset([4, 8, 7], [7, 4, 4, 4, 9, 8])) //true
+console.log(subset([1, 3], [1, 33])) //false
+console.log(subset([1, 3, 10], [10, 8, 8, 8])) //false
---
+++ orion-livecode-2/3.js
@@ -0,0 +1,61 @@
+/**
+Reverse Image 
+
+Diberikan sebuah array 2 dimensi yang berisi angka. Tugas kalian adalah mengubah isi dari elemen-elemen 
+array tersebut.
+Yang harus diubah ada 3 hal:
+1. Urutan dari array di dalam array-2-dimensi harus dibalik, jadi array di paling akhir harus ditaruh di paling awal.
+2. Urutan dari item di dalam array harus diubah urutannya, jadi item yang berada di paling kanan akan berada di 
+paling kiri.
+3. Jika ada angka 0 pada index tersebut, maka angka tersebut harus ditukar dengan angka 1 dan begitu juga sebaliknya
+
+Lihatlah test-case untuk contoh kasus
+
+RULES:
+-Dilarang menggunakan built-in function selain .push()
+
+*/
+  
+function reverse(arr){
+    //implementasi function disini 
+    arr= []
+    for(var i = 0; i < arr.length; i++)
+      for(var j = 0; j < i; j++)
+      var temp = []
+      if(arr[i] > arr[j]);{
+        arr[i] = temp;
+        temp = arr[j];
+        arr[j] = temp
+      }
+    let
+}
+
+
+console.log(reverse(
+    [[1, 2, 3],
+    [0, 1, 5],
+    [0, 3, 1]]));
+/**
+[ [ 0, 3, 1 ], 
+  [ 5, 0, 1 ], 
+  [ 3, 2, 0 ] ]
+ */
+
+console.log(reverse([[1,2,3],[4,5,6],[7,8,9]])); 
+/*
+[ [ 9, 8, 7 ], 
+  [ 6, 5, 4 ], 
+  [ 3, 2, 0 ] ] */
+
+console.log(reverse([[1,2,3,4], [0,1,2,3,4], [0,0,1,3,4], [1,3,4,5,6,7,8]]))
+/*
+[
+  [ 8, 7, 6, 5,
+    4, 3, 0 ],
+  [ 4, 3, 0, 1, 1 ],
+  [ 4, 3, 2, 0, 1 ],
+  [ 4, 3, 2, 0 ]
+]
+*/
+
+console.log(reverse([])); //[]
